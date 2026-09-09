b3f2d5b03d105d0a23955ad1c3ed8443d1071b5a
Jamshid <jamshidabduraxmonov164@gmail.com>
2026-09-10 03:52:35 +0400

    feat: add type definition for fetched data

---
+++ src/types/job.ts
@@ -0,0 +1,30 @@
+export type Job = {
+    id: string;
+    title: string;
+    company:string | null;
+    city: string | null;
+    industry: string | null;
+    description: string | null;
+    sourceURL: string | null;
+    postedAt: string | null;
+    expiresAt: string | null;
+    categories: string[];
+    fresherFriendly: boolean;
+    fresherScore: number;
+};
+
+
+export type Pagination = {
+    page: number;
+    limit: number;
+    totalJobs: number;
+    totalPages: number;
+    returnedJobs: number;
+    hasNextPage: boolean;
+    hasPreviousPage: boolean;
+};
+
+export type JobsResponse = {
+    jobs: Job[];
+    pagination: Pagination;
+};
