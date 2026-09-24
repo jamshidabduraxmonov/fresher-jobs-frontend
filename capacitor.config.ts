d82ee19c1f76ba0d131d54ac78fc8fae17d743b3
Jamshid <jamshidabduraxmonov164@gmail.com>
2026-09-25 03:20:09 +0400

    feat: install enable back button activation in android app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.madebyjamshid.fresherjobs',
+  appName: 'Fresher Jobs UAE',
+  webDir: 'dist'
+};
+
+export default config;
