3d41c1a117be15ef98f8e81cbf470c9400f386fd
Jamshid <jamshidabduraxmonov164@gmail.com>
2026-09-25 04:21:19 +0400

    feat: implement mobile app icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
 import CategoryJobsPage from "./pages/CategoryJobsPage.tsx";
-import { Routes, Route, Link } from "react-router"
+import { Routes, Route, Link, useNavigate } from "react-router";
 import HomePage  from "./pages/HomePage.tsx"
 import JobDetailsPage from "./pages/JobDetailsPage.jsx"
+import { useEffect } from "react";
+import { Capacitor } from "@capacitor/core";
+import { App as CapacitorApp } from "@capacitor/app";
 
 
 function App() {
+
+    const navigate = useNavigate();
+
+    useEffect(()=> {
+        if(Capacitor.getPlatform() !== "android") return;
+
+        const listener = CapacitorApp.addListener(
+            "backButton",
+            ({ canGoBack }) => {
+                if(window.location.pathname === "/") {
+                    void CapacitorApp.minimizeApp();
+                } else if (canGoBack) {
+                    navigate(-1);
+                }else {
+                    navigate("/", { replace: true });
+                }
+            }
+        );
+
+        return () => {
+            void listener.then(handle => handle.remove());
+        };
+    }, [navigate]);
+
   return(
       <Routes>
           <Route
